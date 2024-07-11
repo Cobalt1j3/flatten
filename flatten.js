eeeb3409d892cda6a674524bd7eedaa058606095
Cobalt1j3 <148890393+Cobalt1j3@users.noreply.github.com>
2024-07-11 13:59:17 +0900

    Add files via upload

--- flatten.js
+++ flatten.js
@@ -1,8 +1,6 @@
-function flatten(keyValues, wm = new WeakMap()) {
+function flatten(keyValues) {
 	
 	if (keyValues && typeof keyValues === 'object') {
-		
-		if (wm.has(keyValues)) return false;
 		
 		const	{ assign, getOwnPropertySymbols, keys } = Object,
 				{ isArray } = Array,
@@ -13,16 +11,14 @@ function flatten(keyValues, wm = new WeakMap()) {
 				tmp = [];
 		let i,i0,l0,i1,l1,i2,l2,l3,l4,l5, k,v,v0;
 		
-		i = -1, wm.set(keyValues, results);
+		i = -1;
 		while (++i < length) {
 			
 			i0 = -1, l0 = (isArray(v = keyValues[k = ks[i]]) ? v : (v = [ v ])).length,
 			l4 = 0, l1 = results.length || 1;
 			while (++i0 < l0) {
 				
-				if ((v0 = flatten(v[i0], wm)) === false) return wm.get(v[i0]);
-				
-				i1 = -1, l2 = v0.length, l3 = 0;
+				i1 = -1, l2 = (v0 = flatten(v[i0])).length, l3 = 0;
 				while (++i1 < l1) {
 					
 					i2 = -1, tmp.length = 0;
